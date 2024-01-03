5d68130be1bdd4c9c1c48e4eb51680c99b03e423
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-03 17:14:24 -0600

    se crean vistas en core/views y sus paths en webempresa/urls

--- core/views.py
+++ core/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
 # Create your views here.
+"""
+
+Inicio home/
+Historia about/
+Servicios services/
+Visitanos store/
+Contacto contact/
+Blog blog/
+Sample sample/
+
+"""
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse("Inicio")
+
+
+def about(request):
+    return HttpResponse("Hitoria")
+
+
+def services(request):
+    return HttpResponse("Servicios")
+
+
+def store(request):
+    return HttpResponse("Visitanos")
+
+
+def contact(request):
+    return HttpResponse("Contacto")
+
+
+def blog(requests):
+    return HttpResponse("Blog")
+
+
+def sample(requests):
+    return HttpResponse("Sample")
--- webempresa/urls.py
+++ webempresa/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Paths del core
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('services/', views.services, name="services"),
+    path('store/', views.store, name="store"),
+    path('contact/', views.contact, name="contact"),
+    path('blog/', views.blog, name="blog"),
+    path('sample/', views.sample, name="sample"),
+    # Paths del admin
+
 ]
