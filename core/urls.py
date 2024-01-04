6be9394d9ea20d9e49adbf5e13aba4ca40fd9e9c
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-03 20:33:59 -0600

    Including another URLconf

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('services/', views.services, name="services"),
+    path('store/', views.store, name="store"),
+    path('contact/', views.contact, name="contact"),
+    path('blog/', views.blog, name="blog"),
+    path('sample/', views.sample, name="sample"),
+]
--- webempresa/urls.py
+++ webempresa/urls.py
@@ -14,19 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     # Paths del core
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('services/', views.services, name="services"),
-    path('store/', views.store, name="store"),
-    path('contact/', views.contact, name="contact"),
-    path('blog/', views.blog, name="blog"),
-    path('sample/', views.sample, name="sample"),
+    path('', include('core.urls')),
     # Paths del admin
-
+    path('admin/', admin.site.urls),
 ]
