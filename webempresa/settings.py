c465d282ce15c13a5fe9d6cc946ff9a73a5b418c
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-05 00:01:50 -0600

    se conf Media

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django',
     'core',
+    'services',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media config
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- webempresa/urls.py
+++ webempresa/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
 
 urlpatterns = [
     # Paths del core
@@ -23,3 +23,8 @@ urlpatterns = [
     # Paths del admin
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
