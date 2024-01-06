086edaff090576796ee214536c508a43675321a1
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-05 20:45:34 -0600

    mod de blog/admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Category, Post
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('title', 'author', 'published')
+    ordering = ('author', 'published')
+    search_fields = ('title', 'content', 'author__username',)
+    date_hierarchy = 'published'
+    list_filter = ('author__username', 'categories__name',)
+
+    def post_categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all().order_by("name")])
+    post_categories.short_description = "Categorias"
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
