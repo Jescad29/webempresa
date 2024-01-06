5a5e9260b43901c2781b4b52d4d0c563fe621fc1
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-05 23:15:05 -0600

    creacion de vistas 2

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
 
     path('', views.blog, name="blog"),
+    path('category/<int:category_id>/', views.category, name="category")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
 # Create your views here.
 
 
 def blog(requests):
     posts = Post.objects.all()
     return render(requests, 'blog/blog.html', {'posts': posts})
+
+
+def category(requests, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    posts = Post.objects.filter(categories=category)
+    return render(requests, 'blog/category.html', {'category': category, 'posts': posts})
