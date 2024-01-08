e445061422fab363e3a90d157db94edd820fdced
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-07 21:51:34 -0600

    ckeditor-blog

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 
@@ -22,7 +23,7 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Tìtulo")
-    content = models.TextField(verbose_name="Contenido")
+    content = RichTextField(verbose_name="Contenido")
     published = models.DateTimeField(
         verbose_name="Fecha de publicacion", default=now)
     image = models.ImageField(verbose_name="Imagen",
--- services/models.py
+++ services/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
@@ -6,7 +7,7 @@ from django.db import models
 class Service(models.Model):
     title = models.CharField(max_length=200, verbose_name="titulo")
     subtitle = models.CharField(max_length=200, verbose_name="subtitulo")
-    content = models.TextField(verbose_name="contenido")
+    content = RichTextField(verbose_name="contenido")
     image = models.ImageField(verbose_name="imagen", upload_to="services")
     created = models.DateTimeField(
         auto_now_add=True, verbose_name="fecha de creacion")
