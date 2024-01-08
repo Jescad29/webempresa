d65a025d9548c69a2a7500775595cd1fe1393d34
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-07 19:49:11 -0600

    Creacion de Tags propios

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('<int:page_id>/', views.pages, name="page"),
+    path('<int:page_id>/<slug:page_slug>/', views.pages, name="page"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,6 @@ from .models import Page
 # Create your views here.
 
 
-def pages(request, page_id):
+def pages(request, page_id, page_slug):
     page = get_object_or_404(Page, id=page_id)
     return render(request, "pages/sample.html", {'page': page})
