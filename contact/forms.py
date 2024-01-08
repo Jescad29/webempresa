9f47909dd83b41edf6a23123ab1cedc87cf6e4d3
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-08 17:04:35 -0600

    fusion formulario

--- contact/forms.py
+++ contact/forms.py
@@ -2,7 +2,14 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label="Nombre", required=True)
-    email = forms.EmailField(label="Email", required=True)
+    name = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Escribe tu nombre'},
+    ), min_length=3, max_length=100)
+    email = forms.EmailField(label="Email", required=True, widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'placeholder': 'Escribe tu email'},
+    ))
     content = forms.CharField(
-        label="Contenido", required=True, widget=forms.Textarea)
+        label="Contenido", required=True, widget=forms.Textarea(
+            attrs={'class': 'form-control', 'rows': 4,
+                   'placeholder': 'Escribe tu mensaje'},
+        ), min_length=10, max_length=1000)
